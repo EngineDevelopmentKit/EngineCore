4eeb935fe2ec27330b133df688427df328ae9ed1
koen visscher <koen.visscher@hotmail.com>
2016-10-10 16:39:13 +0200

    abstract window

---
+++ engineCore/include/window/abstract/window.h
@@ -0,0 +1,76 @@
+/**
+* @cond ___LICENSE___
+*
+* Copyright (c) 2016 Koen Visscher, Paul Visscher, Andrew Mac and individual contributors.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+* @endcond
+*/
+
+#pragma once
+#ifndef __EDK_WINDOW_H__
+#define __EDK_WINDOW_H__
+
+#include "common/types.h"
+
+#include "math/scalar/vec2i.h"
+
+#include "window/abstract/windowHandle.h"
+
+#include <string>
+
+namespace EDK
+{
+    class Window
+    {
+    public:
+    
+        Window();
+       
+        virtual void Show() = 0;
+        virtual void Hide() = 0;
+        virtual void Close() = 0;
+        virtual void Open( const Vec2I &size, const std::string &title ) = 0;
+        
+        virtual bool IsOpen() const = 0;
+        
+        virtual Vec2I GetSize() const = 0;
+        virtual Vec2I GetPosition() const = 0;
+        
+        virtual void SetSize( const Vec2I &v ) = 0;
+        virtual void SetPosition( const Vec2I &v ) = 0;
+        
+        virtual void SetMarginedMode( S32 mode ) = 0;
+        virtual void SetFullScreenMode( S32 mode ) = 0;
+        virtual void SetBorderlessMode( S32 mode ) = 0;
+        
+        NativeWindowHandle GetNativeWindowHandle() const = 0;
+        
+    protected:
+        
+        virtual void OnShow() = 0;
+        virtual void OnHide() = 0;
+        virtual void OnClose() = 0;
+        virtual void OnResize() = 0;
+        virtual void OnReposition() = 0;
+    }
+}
+
+#endif
---
+++ engineCore/include/window/abstract/windowHandle.h
@@ -0,0 +1,72 @@
+/**
+* @cond ___LICENSE___
+*
+* Copyright (c) 2016 Koen Visscher, Paul Visscher, Andrew Mac and individual contributors.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+* @endcond
+*/
+
+#pragma once
+#ifndef __EDK_WINDOW_H__
+#define __EDK_WINDOW_H__
+
+#include "common/types.h"
+
+#include "preproc/os.h"
+
+#if OS_IS_WINDOWS
+
+    struct HWND__;
+
+    namespace EDK
+    {
+        typedef HWND__* NativeWindowHandle;
+    }
+
+#elif OS_IS_LINUX
+
+    namespace EDK
+    {
+        typedef U64 NativeWindowHandle;
+    }
+
+#elif OS_IS_MACOS
+
+    namespace EDK
+    {
+        typedef void* NativeWindowHandle;
+    }
+
+#elif OS_IS_ANDROID
+    
+    namespace EDK
+    {
+        typedef void* NativeWindowHandle;
+    }
+
+#elif OS_IS_IOS
+
+    namespace EDK
+    {
+        typedef void* NativeWindowHandle;
+    }
+
+#endif
